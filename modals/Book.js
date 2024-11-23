7191ce3c22cbf557499e88d96aeafafc31958ee8
AnshulGupta <anshulgupta2028@gmail.com>
2024-11-23 06:12:22 +0530

    virtuals in mongoose

--- modals/Book.js
+++ modals/Book.js
@@ -1,11 +1,23 @@
 const mongoose = require("mongoose");
 
-const newBook = new mongoose.Schema({
-  name: { type: String, required: true },
-  desc: { type: String, required: true },
-  price: { type: Number, required: true },
-  auther: { type: String, required: true },
-  LaunchedAt: { type: Date, default: Date.now },
+const newBook = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    desc: { type: String, required: true },
+    price: { type: Number, required: true },
+    auther: { type: String, required: true },
+    LaunchedAt: { type: Date, default: Date.now },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// virtuals
+
+newBook.virtual("discountedPrice").get(function () {
+  return this.price - this.price * (8 / 100);
 });
 
 module.exports = mongoose.model("Book", newBook);
